c843f671643d30f6a9f19680d94f2f61a959cc67
Joy Adhikary <91140113+joy-adhikary@users.noreply.github.com>
2022-09-06 07:58:00 +0600

    Time: 19 ms (57.26%), Space: 18.5 MB (36.76%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         
-        set<int>joy;
+       int i=0;
         
-        for(int i=0;i<nums.size();i++){
-            joy.insert(nums[i]);
+        for(int j=1;j<nums.size();j++){
+            
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
         }
-        
-        int j=0;
-        
-        for(int x:joy){
-            nums[j++]=x;
-        }
-        
-        return joy.size(); 
+       return i+1;
     }
 };
