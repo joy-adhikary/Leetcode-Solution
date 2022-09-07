9b3cbe72953e4434c1c5797d9885eef0d9813c40
Joy Adhikary <91140113+joy-adhikary@users.noreply.github.com>
2022-09-07 08:34:05 +0600

    updated

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -6,12 +6,15 @@ public:
         k=k%n;
         
         // reverse first n-k index for example 1 2 3 4  k=2 , then 2 1 3 4
+        //  Index i (0 <= i < n - k) becomes n - k - i.
         reverse(v.begin(),v.begin()+n-k);
         
         // reverse the rest part ,  2 1 3 4 => 2 1 4 3 
+        //Index i (n-k<= i < n) becomes n - i.
         reverse(v.begin()+n-k,v.end());
         
         // reverse the full array , 2 1 4 3 => 3 4 1 2
+        //index i (0 <= i < n) 
         reverse(v.begin(),v.end());
             
      }
