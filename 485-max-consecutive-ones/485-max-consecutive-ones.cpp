e10bd32655a4d5c241a0465bd4d89d691d49057c
Joy Adhikary <91140113+joy-adhikary@users.noreply.github.com>
2022-09-10 09:37:24 +0600

    Time: 114 ms (5.23%), Space: 36.2 MB (22.50%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -3,13 +3,14 @@ public:
     int findMaxConsecutiveOnes(vector<int>& v) {
        
         int maxi=0,count=0;
-        for(auto x:v){
-            if(x==1){
-                maxi=max(maxi,++count);
-                
-            }
-            else count=0;
-        }
+      for(int i=0;i<v.size();i++){
+          if(v[i]==1)count++;
+          
+          else count=0;
+          
+          maxi=max(maxi,count);
+          
+      }
         return maxi;
     }
 };
