ae785d955aa74bb35ac000e2edc2baad298f166a
Joy Adhikary <91140113+joy-adhikary@users.noreply.github.com>
2022-09-16 21:21:29 +0600

    Time: 71 ms (9.25%), Space: 31 MB (87.40%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& v) {
-        vector<int>v1;
         
-    for(int i=0;i<v.size();i++){
-       v1.push_back(v[i]);
-    }
+//         vector<int>v1;
         
-    for(int i=1;i<v.size();i++){
-        v[i]=v[i-1]+v[i];
-    }
-        int k=v[v.size()-1];
-        int left=0,right=0;
+//     for(int i=0;i<v.size();i++){
+//        v1.push_back(v[i]);
+//     }
         
-        for(int j=0;j<v.size();j++){
+//     for(int i=1;i<v.size();i++){
+//         v[i]=v[i-1]+v[i];
+//     }
+//         int k=v[v.size()-1];
+//         int left=0,right=0;
+        
+//         for(int j=0;j<v.size();j++){
             
-            left=v[j]-v1[j];
-            right=v[v.size()-1]-left-v1[j];
+//             left=v[j]-v1[j];
+//             right=v[v.size()-1]-left-v1[j];
             
-            if(left==right){
-                return j;
-            }
+//             if(left==right){
+//                 return j;
+//             }
             
-        }
+//         }
+//         return -1;
+//     }
+     
+        //with out extra space 
+
+ int right_sum=0,left_sum=0;
+        
+ for(int i=0;i<v.size();i++){
+       right_sum+=v[i];
+   }
+        
+  for(int i=0;i<v.size();i++){
+       right_sum=right_sum-v[i];
+      
+      if(right_sum==left_sum){
+          return i;
+      }
+      
+      left_sum+=v[i];
+   }
+   
         return -1;
     }
 };
