1b51c7000014825dedba8c64b0f9677cbb631d78
Joy Adhikary <91140113+joy-adhikary@users.noreply.github.com>
2022-09-19 21:22:49 +0600

    Time: 40 ms (30.94%), Space: 19.5 MB (59.68%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& v)
+    int majorityElement(vector<int>& ve)
     {
         
        // way 1    time - O(n) space - O(n/2) ==> O( n )
@@ -23,9 +23,27 @@ public:
         
         // The majority element is the element that appears more than ⌊n / 2⌋ times.            so if i sort the whole array then the majority element will be in the mid            point as it appears more then n/2+1 
         
-        int n=v.size();
-        sort(v.begin(),v.end());
-        return v[n/2];
+//         int n=v.size();
+//         sort(v.begin(),v.end());
+//         return v[n/2];
+        
+        
+        //way 3 time - O(n) space - O(1) 
+        
+        int v=0,c=0;
+        for(auto x:ve)
+        {
+            if(v==0)
+            {
+                c=x;
+            }
+            
+            if(c==x)v++;
+            else v--;
+            
+        }
+        return c;
+        
         
     }
 };
